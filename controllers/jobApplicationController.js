111b5686ff0e2f6b2650d971fec5b8f7903d1bb1
Nilo18 <nikalongurashvili@gmail.com>
2026-01-15 22:25:04 +0400

    Added job application update controller

--- controllers/jobApplicationController.js
+++ controllers/jobApplicationController.js
@@ -16,15 +16,15 @@ async function addAppliedJob(req, res, next) {
         const { company_name, date_sent, status } = req.body
 
         if (!company_name) {
-            return res.status(401).json({status: 401, message: 'Please provide company name.'})
+            return res.status(400).json({status: 400, message: 'Please provide company name.'})
         }
 
         if (!date_sent) {
-            return res.status(401).json({status: 401, message: 'Please provide the date on which the application was sent.'})
+            return res.status(400).json({status: 400, message: 'Please provide the date on which the application was sent.'})
         }
 
         if (!status) {
-            return res.status(401).json({status: 401, message: 'Please provide the current status of the application.'})
+            return res.status(400).json({status: 400    , message: 'Please provide the current status of the application.'})
         }
 
         const newJobApp = await JobApplication.create({
@@ -41,12 +41,52 @@ async function addAppliedJob(req, res, next) {
     }
 }
 
+const allowedStatuses = ['Pending', 'Accepted', 'Rejected']
+async function editJobApplication(req, res, next) {
+    try {
+        const { id, field, newValue } = req.body
+
+        if (!id) {
+            return res.status(400).json({status: 400, message: 'Please provide a job application id.'})
+        }
+
+        if (!field) {
+            return res.status(400).json({status: 400, message: 'Please provide a field which you want to edit.'})
+        }
+
+        if (!newValue) {
+            return res.status(400).json({status: 400, message: 'Please provide the new value for the field.'})
+        }
+
+        if (field.trim() === 'status' && !allowedStatuses.includes(newValue)) {
+            return res.status(400).json({status: 400, message: 'Please provide a valid status value.'})
+        }
+
+        console.log(`Changing '${field}' with '${newValue}'...`)
+        const updatedDocument = await JobApplication.findByIdAndUpdate(
+            id,
+            {$set: {[field]: newValue}},
+            {new: true},
+            {runValidators: true}
+        )
+
+        if (!updatedDocument) {
+            return res.status(404).json({status: 404, message: "A job application with the given id couldn't be found"})
+        }
+
+        return res.status(200).json({status: 200, jobApp: updatedDocument})
+    } catch (error) {
+        console.log("Couldn't edit job application: ", error)
+        return res.status(500).json({status: 500, message: error.message})
+    }
+}
+
 async function deleteJobApplication(req, res, next) {
     try {
         const { id } = req.params
 
         if (!id) {
-            return res.status(401).json({status: 401, message: 'Please provide the id of the job application.'})
+            return res.status(400).json({status: 400, message: 'Please provide the id of the job application.'})
         }
 
         const deletedApplication = await JobApplication.findByIdAndDelete(id)
@@ -59,4 +99,4 @@ async function deleteJobApplication(req, res, next) {
     }
 }
 
-module.exports = { getAppliedJobs, addAppliedJob, deleteJobApplication }
+module.exports = { getAppliedJobs, addAppliedJob, editJobApplication, deleteJobApplication }
--- routes/jobApplicationRoute.js
+++ routes/jobApplicationRoute.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { getAppliedJobs, addAppliedJob, deleteJobApplication } = require('../controllers/jobApplicationController.js')
+const { getAppliedJobs, addAppliedJob, 
+    editJobApplication, deleteJobApplication } = require('../controllers/jobApplicationController.js')
 const jobApplicationRouter = express.Router()
 
 jobApplicationRouter.get('/', getAppliedJobs)
 jobApplicationRouter.post('/', addAppliedJob)
+jobApplicationRouter.patch('/', editJobApplication)
 jobApplicationRouter.delete('/:id', deleteJobApplication)
 
 module.exports = jobApplicationRouter
